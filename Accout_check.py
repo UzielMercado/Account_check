a0dea961cf64fbea157641dbee92edd0fce041ca
UzielMercado <dan6425811@gmail.com>
2022-07-18 17:11:49 -0500

    Python update, removed c++ scripts

---
+++ Accout_check.py
@@ -0,0 +1,88 @@
+import time
+from datetime import datetime
+
+Current_balance = 0
+Balance_change = 0
+Base_Origin_balance = 0
+Non_Approved = 0
+Approved = 0
+Non_Approved_Origin = 0
+
+Approved_list = []
+with open ('ApprovedTXNs.txt', 'rt') as Approved_file:
+	for Lines in Approved_file:
+		Approved_list.append(Lines)
+Approved_origin = len(Approved_list)
+Approved_list.clear()
+
+with open ('Balance.txt', 'rt') as Balance_file:
+	for amount in Balance_file:
+		Base_Origin_balance = amount
+Base_Origin_balance_S = Base_Origin_balance.rstrip()
+Origin_balance = float(Base_Origin_balance_S)
+
+while True:
+
+	with open ('ApprovedTXNs.txt', 'rt') as Approved_file:
+		for lines in Approved_file:
+			Approved_list.append(lines)
+
+	print("----------------------------------------------\n"
+	"		Account status\n"
+	"		--------------\n")
+
+	with open ('Balance.txt', 'rt') as Balance_file:
+		for Amount in Balance_file:
+			Base_Current_balance = Amount
+	Base_Current_balance_S = Base_Current_balance.rstrip()
+	Current_balance = float(Base_Current_balance_S)
+
+	if Current_balance != Origin_balance:
+		Balance_change = abs(Current_balance - Origin_balance)
+		Non_Approved += Balance_change
+		Origin_balance = Current_balance
+
+	if Approved_origin < len(Approved_list):
+		New_accounts1 = Approved_list[Approved_origin]
+		New_accounts2 = New_accounts1.split('\n')
+		New_accounts3 = New_accounts2[0].split(' ')
+
+		Approved +=float(New_accounts3[1])
+		Account = New_accounts3[0]
+		if Approved < 0:
+			signals = "----------------------------------------------\nCash out of:" + str(abs(Approved)) + " From: " + str(Account) + "\n----------------------------------------------\n"
+			print(signals)
+			signals_file = open('Signals.txt', 'a')
+			signals_file.write(signals)
+		else:
+			signals = "----------------------------------------------\nSent: " + str(Approved) + " To: " + str(Account) + "\n----------------------------------------------\n"
+			print(signals)
+			signals_file = open('Signals.txt', 'a')
+			signals_file.write(signals)
+	
+	print("\nCurrent balance:")
+	print(Current_balance)
+
+	print("\nLast Balance change:")
+	print(Balance_change)
+
+	if Approved_origin < len(Approved_list):
+		print("\nNon Approved:")
+		Non_Approved -= abs(Approved)
+		print(Non_Approved)
+	else:
+		print("\nNon Approved:")
+		print(Non_Approved)
+
+	if Approved_origin < len(Approved_list):
+		print("\nApproved:")
+		print(Approved)
+		Approved_origin += 1
+		Approved_list.clear()
+	else:
+		print("\nApproved:")
+		print(0)
+		Approved_list.clear()
+
+	print("\n----------------------------------------------\n")
+	time.sleep(.5)
---
+++ TXNp.py
@@ -0,0 +1,85 @@
+import random
+import time
+from random import randint
+
+class TXNsPendientes:
+	x = 0
+	y = 0
+	z = 0
+
+	def __init__(self, number, balance, seconds):
+		self.x = number
+		self.y = balance
+		self.z = seconds
+
+	def __str__ (self):	
+		return str(self.x) + " " + str(self.y) + " " + str(self.z)
+
+TXN_list = []
+balance = []
+string_TXN_list = []
+int_TXN_list = []
+TXNsPendiente = []
+
+timer = 1
+
+with open ('TXNs.txt', 'rt') as TXN_file:
+    for line in TXN_file:
+        TXN_list.append(line)
+
+while True:
+
+	with open ('Balance.txt', 'rt') as Balfile:
+	    for lines in Balfile:
+	        balance.append(lines)
+	print('Current balance:')
+	print(balance[0])
+	print('')
+
+	print('New account in...')
+	print(timer)
+	print('')
+	timer -= 1
+	if timer <= 0:
+
+		string_TXN_list.append(random.choice(TXN_list))
+		for word in string_TXN_list:
+				TXN_convertion_list = word.split(" ")
+
+		for string in TXN_convertion_list:
+		    convertion_string = string.replace("\n", "")
+
+		    int_TXN_list.append(convertion_string)
+
+		change = float(balance[0]) + float(int_TXN_list[2])
+
+		final_balance = open("Balance.txt", "w")
+		final_balance.writelines(str(change))
+		final_balance = open("Balance.txt")
+
+		print("New account.\n")
+
+		list_element = TXNsPendientes(int(TXN_convertion_list[0]), float(int_TXN_list[2]), randint(5, 15))
+		TXNsPendiente.append(list_element)
+		timer = randint(5, 15)
+
+	for account in TXNsPendiente:
+		account.z = account.z - 1
+		print('Approved in...')
+		print(account)
+		print('')
+		if account.z <= 0:
+			print("Account approved !\n")
+			my_file = open('ApprovedTXNs.txt', 'a')
+			my_file.write(str(account.x) + " " + str(account.y) + "\n")
+			my_file = open('ApprovedTXNs.txt')
+
+			del TXNsPendiente[0]
+			TXN_convertion_list.clear()
+
+	time.sleep(1)
+
+
+	balance.clear()
+	string_TXN_list.clear()
+	int_TXN_list.clear()
